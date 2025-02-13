d60e425697eea537e9af1eb9c299ea53d07db5ed
gowtham-aracreate <gowtham@aracreate.com>
2025-02-13 17:09:30 +0530

    feat: DB connectivity

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,66 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors');
+const mongoose = require('mongoose')
 
 app.use(cors());
 app.use(express.json());
 
-let users = [];
+let users = []; // replacing this
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/test');
+        console.log("DB connected")
+
+    } catch (err) {
+        console.log("Error while connecting DB")
+    }
+}
+connectDB();
+
+// mongoose.connect('mongodb://localhost:27017/test').then(() => console.log("Mongodb Connected")).catch((err) => console.log(err));
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    valid: Boolean
+})
+
+
+const ContactSchema = new mongoose.Schema({
+    phone: Number,
+    address: String,
+})
+
+
+const User = mongoose.model("user", UserSchema);
+
+
 
 app.get('/users', (req, res) => {
     res.json(users)
+})
+
+
+app.post('/create', async (req, res) => {
+    const userData = req.body;
+    const data = await User.create(
+        {
+            name: userData.name,
+            age: userData.age,
+            valid: userData.valid
+        }
+    );
+    res.send(data)
+})
+
+
+app.get('/get-user', async (req, res) => {
+    const data = await User.findById();
+
+    res.json(data)
 })
 
 app.post('/users', (req, res) => {
@@ -25,4 +77,6 @@ app.post('/users', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-}) 
+})
+
+
